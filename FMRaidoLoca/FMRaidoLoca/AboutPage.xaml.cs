fb922f54d74df791119a0aad3833e6c38699eff8
Raul <VanRaul@outlook.com>
2013-09-12 00:29:18 +0800

    提交

---
+++ FMRaidoLoca/FMRaidoLoca/AboutPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Tasks;
+
+
+namespace FMRaidoLoca
+{
+    public partial class AboutPage : PhoneApplicationPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+            emailLink.Click += new RoutedEventHandler(emailLink_Click);
+        }
+
+        void emailLink_Click(object sender, RoutedEventArgs e)
+        {
+            EmailComposeTask ect = new EmailComposeTask();
+            ect.To = "raulgblanco@live.com";
+            ect.Subject = "电台反馈";
+            ect.Show();
+
+        }
+    }
+}
---
+++ FMRaidoLoca/FMRaidoLoca/Date/Radios.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace FMRaidoLoca.Data
+{
+	public class Radios
+	{
+		public string RadioName
+		{
+			get;
+			set;
+		}
+
+		public double RadioFrequency
+		{
+			get;
+			set;
+		}
+
+		public string Area
+		{
+			get;
+			set;
+		}
+
+        public string Group { get; set; }
+
+	}
+}
